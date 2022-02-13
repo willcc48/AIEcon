a3b67388e7d6af0f010eab076ffe496b3cf5b7c8
willcc48 <william.chase.carlson@gmail.com>
2022-02-13 13:15:33 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# AI_Econ_Policy
+Web dashboard and extension of Salesforce's AI Economist multi-agent reinforcement learning tax policy environment. Streamlit is being used as a Python web framework.
+
+      cd ai-economist
+      pip install -e .
+  
+Typical packages needed like matplotlib, numpy (use conda environment)
+
+      pip install streamlit
+      pip install plotly
+  
+In scripts directory:
+
+      streamlit run main.py
---
+++ scripts/main.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import optimal_tax as optax
+import covid
+
+if __name__ == "__main__":
+
+    st.set_page_config(
+        page_title="MARL Market Policy",
+        page_icon="🧊",
+        initial_sidebar_state="expanded"
+    )
+
+    hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        </style>
+        """
+    st.markdown(hide_menu_style, unsafe_allow_html=True)
+
+    PAGES = {
+        "Optimal Tax": optax,
+        "COVID-19": covid,
+        "Climate": None,
+        "Learn More": None,
+    }
+
+    st.sidebar.title("Settings")
+
+    model_select = st.sidebar.radio("Select Environment", list(PAGES.keys()))
+    st.sidebar.markdown("***")
+
+    iterations = st.sidebar.number_input('Iterations / Steps', value=1000, min_value=1)
+    plt_every = st.sidebar.number_input('Plot Every', value=100, min_value=1)
+    
+    st.sidebar.text("")
+    save = st.sidebar.checkbox("Save simulation to gif?")
+
+    page = PAGES[model_select]
+    page.model_page(iterations, plt_every, save)
+    
